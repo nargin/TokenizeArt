7007f21d24899e36f722e1593edf97b699a3a253
nargin <92982124+nargin@users.noreply.github.com>
2025-01-09 18:29:36 +0100

    Init commit

---
+++ mint/website/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Account from './components/Account.vue'
+import Connect from './components/Connect.vue'
+</script>
+
+<template>
+  <Account />
+  <Connect />
+</template>
